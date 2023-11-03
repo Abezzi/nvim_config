6b908e230517cf4491588c0204cacc4461abb09b
abezzi <abezzi@outlook.cl>
2023-11-03 09:54:03 -0300

    debug: Shift H and L were inverse

--- mappings.lua
+++ mappings.lua
@@ -26,8 +26,8 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-    ["L"] = { "<cmd>bprev<cr>", desc = "Go to previous tab" },
-    ["H"] = { "<cmd>bnext<cr>", desc = "Go to next tab" },
+    ["H"] = { "<cmd>bprev<cr>", desc = "Go to previous tab" },
+    ["L"] = { "<cmd>bnext<cr>", desc = "Go to next tab" },
   },
   t = {
     -- setting a mapping to false will disable it
